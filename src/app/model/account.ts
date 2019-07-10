f1b54289e8a5e43464ba1d5754f9e142c79e22c5
zitxiem1998 <zitxiem1998@gmail.com>
2019-07-10 16:47:15 +0700

    bo cuc thu muc

---
+++ src/app/model/account.ts
@@ -0,0 +1,11 @@
+export class Account {
+    id: number;
+    fullName: String;
+    age: number;
+    username: String;
+    password: String;
+    phone: String;
+    address: String;
+    email: String;
+    gender: String; 
+}
